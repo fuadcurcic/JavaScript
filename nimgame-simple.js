e9164dbc0840159e9c2c0563e01ec7ab9bfcc3a1
fuadcu@maestralsolutions.com <fuadcu@maestralsolutions.com>
2016-10-13 11:03:09 +0200

    Add Array
    Added Array Session 3

--- nimgame-simple.js
+++ nimgame-simple.js
@@ -1,4 +1,16 @@
 function displaynim() {
-    document.write("*");
+    var Row1 = new Array("1", "2", "3");
+    var Row2 = new Array("A", "B", "C");
+
+    document.writeln(Row1);
+    document.writeln(Row2);
+
+    forEach(function(item) {
+        return document.write(Row1[0]);
+    }, this);
+
+
+
+
 
 }
